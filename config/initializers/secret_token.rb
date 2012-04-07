abe1c4ca6755ecbb65b5c5e0e4c15d3bd5e9493f
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-07 16:51:33 -0300

    Initial commit - with devise installed and working plus a scaffold of 'Team'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevisePostgre::Application.config.secret_token = '7e5a9d12cb5a86ee2e7d83a272ed86dec87f71b647bd346814e29f2c54288b9525413392fb3cc2f5c12c6bb7e6fb514cae20179b47d3d09470df5f1b7c9527e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DevisePostgre::Application.config.session_store :cookie_store, key: '_devise_postgre_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DevisePostgre::Application.config.session_store :active_record_store
